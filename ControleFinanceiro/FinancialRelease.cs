f215b3b5360371243b46aa412e5176b10e61dc3b
Lucas Fogliarini <lucasfogliarini@gmail.com>
2022-11-14 18:31:43 -0300

    create seed

---
+++ ControleFinanceiro/FinancialRelease.cs
@@ -0,0 +1,18 @@
+﻿namespace ControleFinanceiro
+{
+    public class FinancialRelease
+    {
+        public int Id { get; set; }
+        public FinancialReleaseType Type { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime ReleaseAt { get; set; }
+        public decimal Value { get; set; }
+        public string? Description { get; set; }
+    }
+
+    public enum FinancialReleaseType
+    {
+        Cost = 0,
+        Income = 1,
+    }
+}
---
+++ ControleFinanceiro/SeedService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleFinanceiro
+{
+    public static class SeedService
+    {
+        public static IEnumerable<FinancialRelease> Seed()
+        {
+            var financialReleases = new List<FinancialRelease>()
+            {
+                new FinancialRelease 
+                { Type = FinancialReleaseType.Cost, ReleaseAt = new DateTime(2022, 8, 29), Description = "Cartão de Crédito", Value = 825.82m },
+                new FinancialRelease
+                { Type = FinancialReleaseType.Cost, ReleaseAt = new DateTime(2022, 8, 29), Description = "Curso C#", Value = 200 },
+                new FinancialRelease
+                { Type = FinancialReleaseType.Income, ReleaseAt = new DateTime(2022, 8, 31), Description = "Salário", Value = 7000 },
+                new FinancialRelease
+                { Type = FinancialReleaseType.Cost, ReleaseAt = new DateTime(2022, 9, 1), Description = "Mercado", Value = 3000 },
+                new FinancialRelease
+                { Type = FinancialReleaseType.Cost, ReleaseAt = new DateTime(2022, 9, 1), Description = "Farmácia", Value = 300 },
+                new FinancialRelease
+                { Type = FinancialReleaseType.Cost, ReleaseAt = new DateTime(2022, 9, 1), Description = "Combustível", Value = 800.25m },
+                new FinancialRelease
+                { Type = FinancialReleaseType.Cost, ReleaseAt = new DateTime(2022, 9, 15), Description = "Financiamento Carro", Value = 900 },
+                new FinancialRelease
+                { Type = FinancialReleaseType.Cost, ReleaseAt = new DateTime(2022, 9, 22), Description = "Financiamento Casa", Value = 1200 },
+                new FinancialRelease
+                { Type = FinancialReleaseType.Income, ReleaseAt = new DateTime(2022, 9, 25), Description = "Freelance Projeto XPTO", Value = 2500 },
+            };
+
+            return financialReleases;
+        }
+    }
+}
